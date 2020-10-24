a97e9ab33a9c41fcab20f361e4c697c4e0f4eae9
Vivian Jiang <vmj5@cornell.edu>
2020-10-24 14:25:10 -0400

    added webcam test

---
+++ buoyDetector/buoysWebcamTest.py
@@ -0,0 +1,29 @@
+from bouysorwater import BuoyDetector
+import cv2
+import numpy as np
+
+vid = cv2.VideoCapture(0)
+
+print("Press q to quit.")
+
+while (True):
+    _, frame = vid.read()  # get a frame from the webcam
+
+    # scale image
+    max_dimension = max(frame.shape)
+    scale = 700 / max_dimension
+    frame = cv2.resize(frame, None, fx=scale, fy=scale)
+
+    bd = BuoyDetector()
+    bd.process(frame)
+
+    contours = bd.find_contours_output
+    found = bd.find_contours_output != None
+
+    cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
+
+    cv2.imshow('buoy detection', frame)
+
+    # press q to quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break

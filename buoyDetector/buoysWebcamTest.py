a965068cb71502f20abc014c4c8723f0be89d0ee
Vivian Jiang <vmj5@cornell.edu>
2020-10-24 16:53:34 -0400

    basic stuff for getting distances maybe

--- buoyDetector/buoysWebcamTest.py
+++ buoyDetector/buoysWebcamTest.py
@@ -1,6 +1,28 @@
 from bouysorwater import BuoyDetector
 import cv2
 import numpy as np
+import time
+
+
+def find_distances(bd, scale):
+    """Calculates distances from each contour and creates list of obstacle distances from camera.
+    Return:
+        A list where each one represents an obstacle distance.
+    """
+    focal_length = 40  # for webcam? Need to change for sailboat's camera when we find that
+    obstacle_size = 1016  # size of a buoy in mm
+    mm_per_pixel = 1/600  # also based on camera, need to figure this out
+
+    distances = []
+
+    for contour in bd.filter_contours_output:
+        center, size, angle = cv2.minAreaRect(contour)
+        width, height = size
+        distances.append(obstacle_size * focal_length /
+                         (max(width, height) * mm_per_pixel))
+
+    return distances
+
 
 vid = cv2.VideoCapture(0)
 
@@ -17,8 +39,10 @@ while (True):
     bd = BuoyDetector()
     bd.process(frame)
 
-    contours = bd.find_contours_output
-    found = bd.find_contours_output != None
+    contours = bd.filter_contours_output
+    found = contours != None
+
+    print(find_distances(bd, scale))
 
     cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
 
